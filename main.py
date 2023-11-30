5009fcdc028ec26f59b2de277f4cfd88b7a6e8c1
flyingjebi <hansang418@gmail.com>
2023-11-30 22:07:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -33,11 +33,11 @@ def parse_args_and_config():
 
     # parse config file
     if not args.test:
-        with open(os.path.join('configs', args.config), 'r') as f:
+        with open(os.path.join('/content/ncsn/configs', args.config), 'r') as f:
             config = yaml.load(f)
         new_config = dict2namespace(config)
     else:
-        with open(os.path.join(args.log, '/content/ncsn/config.yml'), 'r') as f:
+        with open(os.path.join(args.log, 'config.yml'), 'r') as f:
             config = yaml.load(f)
         new_config = config
 
